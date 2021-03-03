ad7806e58bf496d0b29e0907fb87d32eaab1ecf9
Tung Pham <tung.42@gmail.com>
2021-03-03 09:09:36 +0700

    Lai Kep

---
+++ lai-kep/calculator.js
@@ -0,0 +1,87 @@
+// Input Elements
+const amountEl = document.getElementsByName('amount');
+const contributionEl = document.getElementsByName('contribution');
+const durationEl = document.getElementsByName('duration');
+const interestEl = document.getElementsByName('interest');
+
+// Result Elements
+const startingVal = document.querySelectorAll('.starting-val');
+const contributionVal = document.querySelectorAll('.contribution-val');
+const durationVal = document.querySelectorAll('.duration-val');
+const returnVal = document.querySelectorAll('.return-val');
+const finalVal = document.querySelectorAll('.final-val');
+
+// Format Value
+const formatValue = (value, type) => {
+    var formatted = parseFloat(Math.round(value * 100) / 100).toFixed(2);
+
+    switch (type) {
+        case 'VND':
+            var dong = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value);
+            return dong;
+            break;
+        case '%':
+            return formatted + '%';
+            break;
+        default:
+            return formatted
+            break;
+    }
+}
+
+// Display Results
+const displayResults = (amount, duration, totalContribution, totalReturn, finalBalance) => {
+	startingVal[0].innerHTML = amount;
+	contributionVal[0].innerHTML = totalContribution;
+	durationVal[0].innerHTML = duration;
+	returnVal[0].innerHTML = totalReturn;
+	finalVal[0].innerHTML = finalBalance;
+}
+
+// Get Ratio
+const getRatio = (value, max) => {
+    return 100 * (value/max);
+}
+
+// Render Visuals
+const renderVisuals = (amount, totalContribution, totalReturn, finalBalance) => {
+	const startingBar = document.querySelectorAll('.starting-bar');
+	const contributionBar = document.querySelectorAll('.contribution-bar');
+	const returnBar = document.querySelectorAll('.return-bar');
+	const finalBar = document.querySelectorAll('.final-bar');
+
+	startingBar[0].style.width = getRatio(amount, finalBalance) + '%';
+	contributionBar[0].style.width = getRatio(totalContribution, finalBalance) + '%';
+	returnBar[0].style.width = getRatio(totalReturn, finalBalance) + '%';
+	finalBar[0].style.width = getRatio(finalBalance, finalBalance) + '%';
+}
+
+// Calculate
+const calculate = () => {
+	let amount = amountEl[0].value;
+	let contribution = contributionEl[0].value;
+	let duration = durationEl[0].value;
+	let interest = interestEl[0].value;
+	let rate = interest/100;
+	let finalBalance;
+	let totalContribution;
+	let totalReturn;
+
+	// const
+	if (rate) {
+		finalBalance = amount * Math.pow(1 + rate, duration) + contribution * ( (Math.pow(1 + rate, duration) - 1) / rate );
+		totalContribution = contribution * duration;
+		totalReturn = finalBalance - amount - totalContribution;
+	} else {
+		totalContribution = contribution * duration;
+		finalBalance = +amount + totalContribution;
+		totalReturn = finalBalance - amount - totalContribution;
+	}
+
+	renderVisuals(amount, totalContribution, totalReturn, finalBalance);
+	displayResults(formatValue(amount, 'VND'), duration, formatValue(totalContribution, 'VND'), formatValue(totalReturn, 'VND'), formatValue(finalBalance, 'VND'));
+}
+
+
+// On Load
+calculate();
