f1139e3af0f94d5b94a787613316b4b0dde68725
Tung Pham <tung.42@gmail.com>
2021-08-02 10:44:45 +0700

    fix

--- 2048/serviceWorker.js
+++ 2048/serviceWorker.js
@@ -1,16 +1,16 @@
 var cacheName = 'game-pwa'; 
 var filesToCache = [
-  '/css/2048.css',
-  '/js/bind_polyfill.js',
-  '/js/classlist_polyfill.js',
-  '/js/animframe_polyfill.js',
-  '/js/keyboard_input_manager.js',
-  '/js/html_actuator.js',
-  '/js/grid.js',
-  '/js/tile.js',
-  '/js/local_storage_manager.js',
-  '/js/game_manager.js',
-  '/js/application.js'
+  './css/2048.css',
+  './js/bind_polyfill.js',
+  './js/classlist_polyfill.js',
+  './js/animframe_polyfill.js',
+  './js/keyboard_input_manager.js',
+  './js/html_actuator.js',
+  './js/grid.js',
+  './js/tile.js',
+  './js/local_storage_manager.js',
+  './js/game_manager.js',
+  './js/application.js'
 ];
 self.addEventListener('install', function(e) {
   e.waitUntil(
